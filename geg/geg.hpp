782e385235c78a95f9852b758ec8697599ca1bea
abdelhadi <abdelhadims@icloud.com>
2021-09-05 09:03:14 +0200

    basic structure + basic logging system + main entry for apps

---
+++ geg/geg.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "src/app.hpp"
+#include "src/logger.hpp"
+
+//main
+#include "src/main-entry.hpp"
+
---
+++ sandbox/src/sandbox-app.hpp
@@ -0,0 +1,14 @@
+
+#ifndef GEG_SANDBOX_APP_HPP
+#define GEG_SANDBOX_APP_HPP
+
+#include "geg.hpp"
+
+class sandboxApp : public Geg::App{
+public:
+	sandboxApp();
+	~sandboxApp();
+};
+
+
+#endif //GEG_SANDBOX_APP_HPP
